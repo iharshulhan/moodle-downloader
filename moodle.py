03402e2164ce0d98c8453e5de3e6663da2c97251
Ihar Shulhan <Igor.shulgan2013@gmail.com>
2018-08-22 13:42:01 +0300

    Update moodle.py

--- moodle.py
+++ moodle.py
@@ -72,7 +72,6 @@ conf = ConfigParser()
 project_dir = os.path.dirname(os.path.abspath(__file__))
 conf.read(os.path.join(project_dir, 'config.ini'))
 
-root_directory = conf.get("dirs", "root_dir")
 username = conf.get("auth", "username")
 password = conf.get("auth", "password")
 authentication_url = conf.get("auth", "url").strip('\'"')
